f478039a7a96d490fac9631594b763d3c459b14a
hexrajkumar <rajkumar.mahto@hexagonglobal.in>
2020-12-26 19:54:47 +0530

    resolved

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,33 @@
 import React, { useState } from "react";
-import ListItem from './ListItem';
 import "./../styles/App.css";
+import ListItem from './ListItem';
 function App() {
 	const [items, setItems] = useState([]);
 	const [newItem, setNewItem] = useState("");
-	const addItem = () => {
+	const addItem = () =>{
 		items.push(newItem);
 		setItems([...items]);
 		setNewItem("");
-	}
-	const newItemChanged = (evt) => {
+	};
+	const setNewItemChanged = (evt) => {
 		setNewItem(evt.target.value);
-	}
-	const editHandler = (editedValue,itemIdx) => {
-		items[itemIdx] = editedValue;
-		setItems([...items])
-	  }
+	};
 	const deleteHandler = (itemIdx) => {
-	  items.splice(itemIdx, 1);
-	  setItems([...items])
-	}
+		items.splice(itemIdx,1);
+		setItems([...items]);
+	};
+	const editHandler = (editedValue, itemIdx) => {
+		items[itemIdx] = editedValue;
+		setItems([...items]);
+	} 
 	return (
 	<div id="main">
-	<textarea id="task" onChange={newItemChanged} placeholder="New Item" value={newItem}></textarea>
-	<button id="btn" onClick={addItem} 
-	disabled={newItem.trim().length===0}>Add Item</button>
-	{items.map((item,idx)=>(
-		<ListItem item={item} key={`${item}_${idx}`} idx={idx} editHandler={editHandler} deleteHandler={deleteHandler}/>
+	<textarea id="task" onChange={setNewItemChanged} placeholder="New Item" value={newItem}></textarea>
+	<button id="btn" onClick={addItem} disabled={newItem.trim().length===0}>Add Item</button>
+	{items.map((item,idx) =>(
+		<ListItem item={item} key={`${item}_${idx}`} idx={idx} editHandler={editHandler} deleteHandler = {deleteHandler}/>
 	))}
 	</div>
 	);
 }
-
-
 export default App;
